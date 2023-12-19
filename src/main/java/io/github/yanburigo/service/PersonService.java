2a160eff2a0a84d8987c0f5f6cd37146f94d85ba
Yan Burigo <yanburigoribeiro@gmail.com>
2023-12-19 10:40:31 -0300

    Refactoring PersonService Test

--- src/main/java/io/github/yanburigo/service/PersonService.java
+++ src/main/java/io/github/yanburigo/service/PersonService.java
@@ -1,11 +1,19 @@
 package io.github.yanburigo.service;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import io.github.yanburigo.modal.Person;
 
 public class PersonService implements IPersonService {
 
 	@Override
 	public Person createPerson(Person person) {
+		if(person.getEmail() == null || person.getEmail().isBlank()) {
+			throw new IllegalArgumentException("The Person email is null or empty!");
+		}
+		
+		var id = new AtomicLong().incrementAndGet();
+		person.setId(id);
 		return person;
 	}
 
--- src/test/java/io/github/yanburigo/PersonServiceTest.java
+++ src/test/java/io/github/yanburigo/PersonServiceTest.java
@@ -13,18 +13,17 @@ import io.github.yanburigo.service.PersonService;
 public class PersonServiceTest {
 
 	Person person;
+	IPersonService service;
 
 	@BeforeEach
 	void setup() {
 		person = new Person("Keith", "Moon", "yan.burigo@outlook.com", "Wembley - UK", "Male");
+		service = new PersonService();
 	}
 
 	@DisplayName("When Create a Person with Success Should Return a Person Object")
 	@Test
 	void testCreatePerson_whenSuccess_ShouldReturnPersonObject() {
-
-		// Given / Arrange
-		IPersonService service = new PersonService();
 
 		// When / Act
 		Person actual = service.createPerson(person);
@@ -33,21 +32,33 @@ public class PersonServiceTest {
 		assertNotNull(actual, () -> "The createPerson() should not have returned null!");
 	}
 
-	@DisplayName("When Create a Person with Success Should Contains FirstName In Returned Person Object")
+	@DisplayName("When Create a Person with Success Should Contains Valid Fields In Returned Person Object")
 	@Test
-	void testCreatePerson_whenSuccess_ShouldContainsFirstNameInReturnedPersonObject() {
-
-		// Given / Arrange
-		IPersonService service = new PersonService();
+	void testCreatePerson_whenSuccess_ShouldContainsValidFieldsInReturnedPersonObject() {
 
 		// When / Act
 		Person actual = service.createPerson(person);
 
 		// Then / Assert
+		assertNotNull(person.getId(), () -> "Person ID is Missing");
 		assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The Person FistName is Incorrect!");
 		assertEquals(person.getLastName(), actual.getLastName(), () -> "The Person LastName is Incorrect!");
 		assertEquals(person.getAddress(), actual.getAddress(), () -> "The Person Address is Incorrect!");
 		assertEquals(person.getGender(), actual.getGender(), () -> "The Person Gender is Incorrect!");
 		assertEquals(person.getEmail(), actual.getEmail(), () -> "The Person Email is Incorrect!");
+	}
+	
+	@DisplayName("When Create a Person whith Null Email Should Throw Illegal Argument Exception")
+	@Test
+	void testCreatePerson_whithNullEmail_ShouldThrowIllegalArgumentException() {
+
+		// Given / Arrange
+		person.setEmail(null);
+		
+		var expectedMessage = "The Person email is null or empty!";
+
+		// When / Act & Then / Assert
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> service.createPerson(person), () -> "Empty Email should have cause an IllegalArgumentException!");
+		assertEquals(expectedMessage, exception.getMessage(), () -> "Exception error message is incorrect!");
 	}
 }
