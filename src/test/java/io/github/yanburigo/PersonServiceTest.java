1c93c0b71482bb53a77e273bbdc9ee077b9da7bb
Yan Burigo <yanburigoribeiro@gmail.com>
2023-12-19 10:25:16 -0300

    Enhanced assertions

--- src/test/java/io/github/yanburigo/PersonServiceTest.java
+++ src/test/java/io/github/yanburigo/PersonServiceTest.java
@@ -1,5 +1,8 @@
 package io.github.yanburigo;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,18 +12,42 @@ import io.github.yanburigo.service.PersonService;
 
 public class PersonServiceTest {
 
+	Person person;
+
+	@BeforeEach
+	void setup() {
+		person = new Person("Keith", "Moon", "yan.burigo@outlook.com", "Wembley - UK", "Male");
+	}
+
 	@DisplayName("When Create a Person with Success Should Return a Person Object")
 	@Test
 	void testCreatePerson_whenSuccess_ShouldReturnPersonObject() {
-		
+
 		// Given / Arrange
 		IPersonService service = new PersonService();
-		
-		Person person = new Person("Keith", "Moon", "yan.burigo@outlook.com", "Wembley - UK", "Male");
-		
+
 		// When / Act
 		Person actual = service.createPerson(person);
-		
+
 		// Then / Assert
+		assertNotNull(actual, () -> "The createPerson() should not have returned null!");
+	}
+
+	@DisplayName("When Create a Person with Success Should Contains FirstName In Returned Person Object")
+	@Test
+	void testCreatePerson_whenSuccess_ShouldContainsFirstNameInReturnedPersonObject() {
+
+		// Given / Arrange
+		IPersonService service = new PersonService();
+
+		// When / Act
+		Person actual = service.createPerson(person);
+
+		// Then / Assert
+		assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The Person FistName is Incorrect!");
+		assertEquals(person.getLastName(), actual.getLastName(), () -> "The Person LastName is Incorrect!");
+		assertEquals(person.getAddress(), actual.getAddress(), () -> "The Person Address is Incorrect!");
+		assertEquals(person.getGender(), actual.getGender(), () -> "The Person Gender is Incorrect!");
+		assertEquals(person.getEmail(), actual.getEmail(), () -> "The Person Email is Incorrect!");
 	}
 }
